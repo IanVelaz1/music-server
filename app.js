362f137c59ae25e4c8370b7c29f19ebcd49a8f3e
jansolote <iancalidadtnsgcc@gmail.com>
2019-01-04 14:01:32 -0600

    songs added

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
-const express=require("express"),
-app=express(),
-bodyParser=require('body-parser'),
-cors=require('cors'),
-helmet=require("helmet"),
-fs=require("fs");
+const express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    helmet = require("helmet"),
+    fs = require("fs");
 
-const port=process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 
 ///middlewares
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 app.use(helmet());
 
 
 ///routes
-require("./routes/musicRoutes")(app,fs);
+require("./routes/musicRoutes")(app, fs);
 
 
-app.listen(port,(error,success)=>{
- console.log("app conectada en puerto: "+port);
+
+app.listen(port, (error, success) => {
+    console.log("app conectada en puerto: " + port);
 });
--- routes/musicRoutes.js
+++ routes/musicRoutes.js
@@ -1,17 +1,36 @@
-module.exports=(app,fs)=>{
+const path = require("path");
+const url = require("url");
+const ms=require("mediaserver");
+module.exports = (app,fs) => {
 
-    app.get('/track/:id',(req,res)=>{
+    app.get("/track/:id", (req, res) => {
         const id=req.params.id;
-        const trackPath="D:/polymer/musicServer/music/"+id+".mp3"; 
-        console.log(trackPath);
-        
-        if(fs.existsSync(trackPath)){
-            let musicStream=fs.createReadStream(trackPath);
-            musicStream.pipe(res);
-        }else{
-            res.json({error:"file does not exist"})
-            res.end();
-        };
+        let songPath=path.resolve(`../music-server/music/${id}`);
+        ms.pipe(req,res,songPath);
     });
 
+    app.get("/tracks",(req,res)=>{
+        let folderPath=path.resolve("../music-server/music");
+        fs.readdir(folderPath,(error,items)=>{
+            console.log(items);
+            if(!error){
+                res.json({tracks:items,status:200})
+            }
+        });
+    });
 }
+/*
+    app.get('/track/:id',(req,res)=>{
+        const id=req.params.id;
+        let songPath=path.resolve(`../music-server/music/${id}`);
+        const stat=fs.statSync(songPath);
+        let queryData=url.parse(req.url,true);
+        const skip=typeof(queryData.skip)==="undefined"?0:queryData.skip;
+        console.log(queryData);
+        const startByte=stat.size*skip;
+        res.writeHead(200,{
+            'Content-Type':'audio/mp3',
+            'Content-Length':stat.size-startByte
+        });
+        fs.createReadStream(songPath,{start:startByte}).pipe(res)
+    });*/
